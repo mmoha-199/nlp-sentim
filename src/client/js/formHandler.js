9532ef2a61e8576ab3ac9b0ac06d4e752be712bd
mohamed <76411950+mmoha-199@users.noreply.github.com>
2021-08-24 23:37:48 +0100

    fix handelsub

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -3,14 +3,56 @@ function handleSubmit(event) {
 
     // check what text was put into the form field
     let formText = document.getElementById('name').value
-    checkForName(formText)
+    if(checkForName(formText)){
 
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
-    })
+      console.log("::: Form Submitted :::")
+      fetch('http://localhost:8080/addData',{
+          method: 'POST',
+          credentials: 'same-origin',
+          mode: 'cors',
+          headers:{
+            'Content-Type':'application/json',
+          },
+          body:JSON.stringify({formText: formText}),
+        })
+      .then(res => res.json())
+      .then(function(res) {
+        document.getElementById('results').innerHTML = res.message;
+    
+        document.getElementById('confidence').innerHTML = "- Feelings of confidence in this text are given a rating of "+ res.confidence +"%";
+        if (res.score_tag === 'N'){
+        document.getElementById('polarity').innerHTML = "- Overall, the tone is negative." }
+        else if(res.score_tag === 'N+'){
+            document.getElementById('polarity').innerHTML = "- Overall, the tone is very negative."}
+        else if(res.score_tag === 'NONE'){
+            document.getElementById('polarity').innerHTML = "- No sentiment is detected."}
+        else if(res.score_tag === 'P+'){
+            document.getElementById('polarity').innerHTML = "- Overall, the tone is very positive"}
+        else if(res.score_tag === 'NEU'){
+            document.getElementById('polarity').innerHTML = "- Overall, the tone is neutral."}
+         else if(res.score_tag === 'P'){
+            document.getElementById('polarity').innerHTML = "- Overall, the tone is positive."};
+        if (res.agreement === 'AGREEMENT'){
+            document.getElementById('agreement').innerHTML = "- This tone is consistent throughout the text"}
+        else if(res.agreement === 'DISAGREEMENT'){
+            document.getElementById('agreement').innerHTML = "- However, this tone isn't consistent throughout the text"};
+        if(res.subjectivity === 'OBJECTIVE'){
+            document.getElementById('obj').innerHTML = "- Our analysis notes that this article is more objective than subjective."}
+        else if(res.subjectivity === 'SUBJECTIVE'){
+            document.getElementById('obj').innerHTML = "- Our analysis notes that this article is more subjective than objective." }
+        else if(res.subjectivity === null){
+            document.getElementById('obj').innerHTML = ""}; 
+        if(res.irony === 'NONIRONIC'){
+            document.getElementById('ironic').innerHTML = "-We detected no irony."}
+        else if(res.irony === 'IRONIC'){
+            document.getElementById('ironic').innerHTML = "-Also, we detected a level of irony."}
+        else if(res.irony === null){
+            document.getElementById('ironic').innerHTML = ""}  
+        })
+      console.log("::: Form Submitted :::");
+    }else{ alert("Please enter a valid URL");
+        console.log("Not valid url");
+    }
 }
 
 export { handleSubmit }
