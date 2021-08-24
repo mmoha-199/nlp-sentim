8f8c48b2b0e4453bf1d56620eb781aa832be7d58
mohamed <76411950+mmoha-199@users.noreply.github.com>
2021-08-24 23:46:11 +0100

    rename to sass

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,6 @@
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
 
-console.log(checkForName);
+export {checkForName,handleSubmit}
 
-alert("I EXIST")
-console.log("CHANGE!!");
+
